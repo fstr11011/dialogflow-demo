8ad44bf02318cf92d569df09330605690b5cace2
Michael Gladstein <mtg@Michaels-MacBook-Pro-2.local>
2018-08-08 11:31:57 -0400

    update

--- routes.js
+++ routes.js
@@ -45,11 +45,11 @@ router.post("/", function(req, res, next){
                         });
                     } else {
                         res.json({
-                            "fulfillmentText": "No account was found for: " + req.body.parameters.employeeNumber + ".",
-                            //"followupEventInput": {
-                            //    "name": "employee_not_found",
-                            //    "languageCode": "en-US",
-                            //}
+                            "fulfillmentText": "No account was found for: " + req.body.queryResult.parameters.employeeNumber + ".",
+                            "followupEventInput": {
+                                "name": "employee_not_found",
+                                "languageCode": "en-US",
+                            }
                         });
                     }
             });
