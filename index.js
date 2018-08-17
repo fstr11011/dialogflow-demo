48663d22a842be3dbf2bb4425df3361908f0f73b
Michael Gladstein <mtg@Michaels-MacBook-Pro-2.local>
2018-08-17 16:20:30 -0400

    removed request

--- index.js
+++ index.js
@@ -38,9 +38,6 @@ app.use(function(req, res, next){
     next();
 });
 
-request.get('https://api.api.ai/v2/query?v=20150910&e=custom_start&timezone=America/New_York&lang=en&sessionId=1234567890').auth(null, null, true, '96fa0fd9218c4416ba6ccb7c86de38da');
-console.log('passed request get');
-
 app.use("/", routes);
 
 app.use(function(req, res, next){
